f63bb675af6742f8a1ca9dcddd572993a9e02f14
zy414266852 <414266852@qq.com>
2017-03-31 14:25:59 +0800

    新建项目

---
+++ YuYi_Server/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YuYi_Server
+//
+//  Created by wylt_ios_1 on 2017/3/31.
+//  Copyright © 2017年 wylt_ios_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YuYi_Server/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YuYi_Server
+//
+//  Created by wylt_ios_1 on 2017/3/31.
+//  Copyright © 2017年 wylt_ios_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
