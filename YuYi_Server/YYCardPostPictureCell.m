27e788a43955155dad1c632c063c455148729e07
jdcwy <414266852@qq.com>
2017-04-12 15:51:02 +0800

    图片选择器和删除已选图片功能

---
+++ YuYi_Server/YYCardPostPictureCell.m
@@ -0,0 +1,222 @@
+//
+//  YYCardPostPictureCell.m
+//  YuYi_Server
+//
+//  Created by 万宇 on 2017/4/12.
+//  Copyright © 2017年 wylt_ios_1. All rights reserved.
+//
+
+#import "YYCardPostPictureCell.h"
+#import <Masonry.h>
+#import "UILabel+Addition.h"
+#import "UIColor+colorValues.h"
+#import <UIImageView+WebCache.h>
+#import "CcUserModel.h"
+#import "HttpClient.h"
+@interface YYCardPostPictureCell ()
+@property(nonatomic,weak)UIImageView *pictureView;
+
+@end
+
+@implementation YYCardPostPictureCell
+
+-(instancetype)init{
+    if (self = [super init]) {
+        [self setupUI];
+    }
+    return self;
+}
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self setupUI];
+    }
+    return self;
+}
+-(void)setImage:(UIImage *)image{
+    _image = image;
+//    image = [self thumbnailWithImageWithoutScale:image size:CGSizeMake(kScreenW, kScreenW)];
+//    image = [self imageCompressForWidthScale:image targetWidth:kScreenW];
+    image = [self imageCompressFitSizeScale:image targetSize:CGSizeMake(kScreenW, kScreenW)];
+    self.pictureView.image = image;
+    
+}
+//等比例缩放
+- (UIImage *)thumbnailWithImageWithoutScale:(UIImage *)image size:(CGSize)asize
+{
+    UIImage *newimage;
+    if (nil == image)
+    {
+        newimage = nil;
+    } else {
+        CGSize oldsize = image.size;
+        CGRect rect;
+        if (asize.width/asize.height > oldsize.width/oldsize.height)
+        {
+            rect.size.width = asize.height*oldsize.width/oldsize.height;
+            rect.size.height = asize.height;
+            rect.origin.x = (asize.width - rect.size.width)/2;
+            rect.origin.y = 0;
+        } else {
+            rect.size.width = asize.width;
+            rect.size.height = asize.width*oldsize.height/oldsize.width;
+            rect.origin.x = 0;
+            rect.origin.y = (asize.height - rect.size.height)/2;
+        }
+        
+        UIGraphicsBeginImageContext(asize);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+        UIRectFill(CGRectMake(0, 0, asize.width, asize.height));//clear background
+        [image drawInRect:rect];
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    return newimage;
+    
+}
+//按比例缩放,size 是你要把图显示到 多大区域
+- (UIImage *) imageCompressFitSizeScale:(UIImage *)sourceImage targetSize:(CGSize)size{
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = size.width;
+    CGFloat targetHeight = size.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+            
+        }
+        else{
+            
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }else if(widthFactor < heightFactor){
+            
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(size);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if(newImage == nil){
+        NSLog(@"scale image fail");
+    }
+    
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+//指定宽度按比例缩放
+-(UIImage *) imageCompressForWidthScale:(UIImage *)sourceImage targetWidth:(CGFloat)defineWidth{
+    
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = defineWidth;
+    CGFloat targetHeight = height / (width / targetWidth);
+//    CGFloat targetHeight =defineWidth;
+    CGSize size = CGSizeMake(targetWidth, targetHeight);
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+        }
+        else{
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+            
+        }else if(widthFactor < heightFactor){
+            
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(size);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    if(newImage == nil){
+        
+        NSLog(@"scale image fail");
+    }
+    UIGraphicsEndImageContext();  
+    return newImage;  
+}
+- (void)setupUI{
+    //pictureView
+    UIImageView *pictureView = [[UIImageView alloc]init];
+    //    UIViewContentModeScaleToFill,
+    //    UIViewContentModeScaleAspectFit,      // UIViewContentModeCenter
+    //    UIViewContentModeScaleAspectFill,
+    pictureView.contentMode = UIViewContentModeCenter;
+    pictureView.clipsToBounds = true;
+    pictureView.image = [UIImage imageNamed:@"add_pic"];
+    [self.contentView addSubview:pictureView];
+    self.pictureView = pictureView;
+    
+    //约束
+    [pictureView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.bottom.offset(0);
+//        make.left.offset(20*kiphone6);
+//        make.right.offset(-20*kiphone6);
+    }];
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(pictureView.mas_bottom);
+        make.width.offset(kScreenW);//必须加
+    }];
+    
+}
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+
+@end
