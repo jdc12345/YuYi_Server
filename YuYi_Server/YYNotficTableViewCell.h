0f507e5f12d0f8f363f335016d985a9e85d8e033
zy414266852 <414266852@qq.com>
2017-04-12 09:03:57 +0800

    推动消息，挂号更改

---
+++ YuYi_Server/YYNotficTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  YYNotficTableViewCell.h
+//  YuYi_Server
+//
+//  Created by wylt_ios_1 on 2017/4/11.
+//  Copyright © 2017年 wylt_ios_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYNotficTableViewCell : UITableViewCell
+
+@end
---
+++ YuYi_Server/YYNotficTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  YYNotficTableViewCell.m
+//  YuYi_Server
+//
+//  Created by wylt_ios_1 on 2017/4/11.
+//  Copyright © 2017年 wylt_ios_1. All rights reserved.
+//
+
+#import "YYNotficTableViewCell.h"
+
+@implementation YYNotficTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
