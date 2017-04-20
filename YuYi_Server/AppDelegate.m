4fa1c377ec670454834082836413f6320ea51f4d
zy414266852 <414266852@qq.com>
2017-04-20 17:31:54 +0800

    后台 隐藏hud

--- YuYi_Server/AppDelegate.m
+++ YuYi_Server/AppDelegate.m
@@ -250,6 +250,7 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo {
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    [SVProgressHUD dismiss];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
